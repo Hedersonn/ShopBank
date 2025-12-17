6f299685aa3bd54bd47082b682635efabd672403
Hedersonn <rigonatti.hederson@gmail.com>
2025-12-17 12:25:33 -0300

    Basic of the loop

--- src/br/com/hederson/shopbank/main/Main.java
+++ src/br/com/hederson/shopbank/main/Main.java
@@ -3,14 +3,30 @@ package br.com.hederson.shopbank.main;
 import br.com.hederson.shopbank.app.Account;
 import br.com.hederson.shopbank.app.Item;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Account myAccount = new Account(7060);
-        Item phone = new Item("rog phone", (double) 700);
-        myAccount.addItem(phone);
-        Item item2 = new Item("ola", (double) 33);
-        myAccount.addItem(item2);
-        myAccount.sortedItems();
-        System.out.println(myAccount.getItems());
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Credit Limit: ");
+        double limit = scan.nextDouble();
+        Account myAccount = new Account(limit);
+
+        int chooseContinue = 1;
+        while(chooseContinue != 0) {
+
+            System.out.print("Product name: ");
+            String productName = scan.next();
+            System.out.print("Product price: ");
+            double productPrice = scan.nextDouble();
+            Item item = new Item(productName, productPrice);
+            myAccount.addItem(item);
+
+            System.out.println("Continue?\n0 to Stop\n1 to Continue");
+            chooseContinue = scan.nextInt();
+        }
     }
 }
+
+//chooseContinue = scan.nextInt();
